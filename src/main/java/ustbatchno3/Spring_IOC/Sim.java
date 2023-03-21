2007a52d535b5590e7af125d10031ef2265fce13
Christo Shaji <Christo.Shaji@ust.com>
2023-03-21 09:35:39 +0530

    21/03/20233

---
+++ src/main/java/ustbatchno3/Spring_IOC/Sim.java
@@ -0,0 +1,6 @@
+package ustbatchno3.Spring_IOC;
+
+public interface Sim {
+   public void calling();
+   public void dialling();
+}
---
+++ src/main/java/ustbatchno3/Spring_IOC/VI.java
@@ -0,0 +1,16 @@
+package ustbatchno3.Spring_IOC;
+
+public class VI implements Sim{
+
+	public void calling() {
+		// TODO Auto-generated method stub
+		System.out.println("I am calling from the VI sim");
+	}
+
+	public void dialling() {
+		// TODO Auto-generated method stub
+		System.out.println("I am dialling from the VI sim");
+	}
+  
+}
+
